cb3f790f6e4df7e7248fa4f21ff6a404d8b6fec3
Ruslan <ruslan.pershin98@gmail.com>
2022-07-13 20:04:16 +0300

    useMemo

---
+++ src/stories/UseMemo.stories.tsx
@@ -0,0 +1,76 @@
+import React, {useMemo, useState} from "react";
+import {Select} from "../components/Select/Select";
+
+export default {
+    title: 'useMemo'
+}
+
+
+export const DifficultCountingExample = () => {
+    const [a, setA] = useState<number>(0)
+    const [b, setB] = useState<number>(0)
+
+    let resultA = 1;
+    let resultB = 1;
+
+    resultA = useMemo(() => {
+        let tempResultA = 1;
+        for (let i = 1; i <= a; i++) {
+            let fake = 0;
+            while (fake < 100000000) {
+                fake++;
+                const fakeValue = Math.random();
+            }
+            tempResultA = tempResultA * i;
+        }
+        return tempResultA
+    }, [a]);
+
+
+    for (let i = 1; i <= b; i++) {
+        resultB = resultB * i;
+    }
+
+    return <>
+        <input value={a} onChange={(e) => setA(Number(e.currentTarget.value))}/>
+        <input value={b} onChange={(e) => setB(Number(e.currentTarget.value))}/>
+
+        <hr/>
+        <div>Result for a : {resultA}</div>
+        <div>Result for b : {resultB}</div>
+    </>
+}
+
+
+
+const UsersSecret = (props: {users: Array<string>}) => {
+    console.log('USERS')
+    return <div>
+        {props.users.map((u,i) => <div key={i}>{u}</div>)}
+    </div>
+}
+
+const Users = React.memo(UsersSecret)
+
+
+export const HelpsToReactMemo = () => {
+    const [counter, setCounter] = useState(0)
+    const [users, setUsers] = useState(['Dimych', 'Valera', 'Artem'])
+
+    const newArray = useMemo(() => {
+        return users.filter(u=>u.toLowerCase().indexOf('a') > -1)
+    }, [users])
+
+    const addUser = () => {
+        const newUsers = [...users, 'Sveta']
+        setUsers(newUsers)
+    }
+
+    return <>
+        <button onClick={() => setCounter(counter+1)}>+</button>
+        <button onClick={addUser}>add user</button>
+        {counter}
+        <Users users={newArray}/>
+    </>
+}
+
